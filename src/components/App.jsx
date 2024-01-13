0c5137a1f949e5f246675953b30882cf218d41a8
Mariia Samodurova <128381107+MariiaSam@users.noreply.github.com>
2024-01-13 22:57:34 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import { useSelector } from 'react-redux';
+import { selectContacts } from '../redux/selectors';
+
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactsTitle } from './ContactsTitle/ContactsTitle';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export const App = () => {
+  const contacts = useSelector(selectContacts);
+
+  return (
+    <div>
+      <Section title="Phonebook">
+        <ContactForm />
+        {contacts.lengtg > 0 && (
+          <>
+            <ContactsTitle title="Contacts" />
+          </>
+        )}
+        <Filter />
+        <ContactList />
+      </Section>
+    </div>
+  );
+};
